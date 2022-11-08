964e1f630d6f5b6a3f1445bbd459877827b4578e
Rucherd <106265410+Rucherd@users.noreply.github.com>
2022-11-07 23:14:08 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+/**
+ * @author Richard Liu
+ * Date: April 1st 2021
+ * Course: ICS4U7
+ * Teacher: Ms. Xie
+ * ICS Internal Assessment
+ * 
+ * Student Marks Manager
+ * 
+ * Class Main
+ */
+public class Main { 
+	public static void main(String[] args) {  
+		new HomePage(); 
+		//every new window is a separate class, so they can be called and independently and customized
+	}  
+}  
