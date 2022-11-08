e03aafa4766e461ea73c7f534512f740c0deb009
Rucherd <106265410+Rucherd@users.noreply.github.com>
2022-11-07 23:14:24 -0500

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,33 @@
+/**
+ * Node class
+ */
+public class Node{
+	
+      private Object data;
+      private Node next;
+
+      public Node (Object data){
+    	  this.data = data;
+    	  this.next = null;
+      }
+   
+   
+      public void setData (Object data){
+    	  this.data = data;
+      }
+   
+   
+      public void setNext (Node next){
+    	   this.next = next;
+      }
+   
+   
+      public Object getData (){
+    	  return data;
+      }
+      
+      public Node getNext (){
+    	   return next;
+      }
+   
+}
